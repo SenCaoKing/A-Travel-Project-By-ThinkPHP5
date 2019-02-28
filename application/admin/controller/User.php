75fff28e77e2a8eeca7a0dd269910fa033411d03
SenCaoKing <1134349927@qq.com>
2019-02-28 23:54:02 +0800

    导游列表制作

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -1,13 +1,28 @@
 <?php
+/**
+ * Created by PhpStrom.
+ * User: Sen
+ * Date: 2018/12/28
+ * Time: 21:00
+ */
 namespace app\admin\controller;
 
-use think\Controller;
+class User extends Base{
+    public function guideList(){
+        $search = search($this->params, ['mobile'], [], ['status']);
+        dump($search);
+        $where = $search['where'] ?: '';
+        $list = $this->guide
+            ->where($where)
+            ->order('uid desc')
+            ->paginate(10, false, ['query' => $this->params, 'var_page' => 'page']);
 
-class User extends Controller
-{
-    public function userlist()
-    {
-        dump(123);
-        return view('userlist');
+        dump($list);
+        return view('guideList', ['list' => $list, 'params' => $search['params']]);
+    }
+
+
+    public function userlist(){
+        return view('userList');
     }
 }
