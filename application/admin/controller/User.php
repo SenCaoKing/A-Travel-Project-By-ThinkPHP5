22799f4ec9245268568aaacd4615351356e5610f
SenCaoKing <1134349927@qq.com>
2019-03-01 22:53:13 +0800

    导游列表详情及状态操作

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -16,9 +16,43 @@ class User extends Base{
             ->where($where)
             ->order('uid desc')
             ->paginate(10, false, ['query' => $this->params, 'var_page' => 'page']);
-
-        dump($list);
         return view('guideList', ['list' => $list, 'params' => $search['params']]);
+    }
+
+    public function guideShow(){
+        $info = $this->guide
+            ->alias('a')
+            ->field('a.*, b.name, b.id_num, b.guide_card, b.bank_name, b.bank_num, b.positive_id_path, b.negative_id_path, b.hand_id_path, b.guide_card_path, c.drivers, c.travel, c.frame, c.car_type, c.manned, c.plate')
+            ->join([['guide_info b', 'a.uid = b.uid', 'left'], ['drivers c', 'a.uid = c.uid', 'left']])
+            ->where(['a.uid'=>$this->params['uid']])
+            ->find();
+        $labels = $this->guide_label
+            ->column('label');
+        $info['label'] = implode(' ', $labels);
+        return view('guideShow', ['info'=>$info]);
+    }
+
+    public function guideStatus(){
+        if(IS_AJAX){
+            try{
+                $status = $this->params['status'] == 2 ? 4 : 2;
+                $this->guide->update(['status'=>$status, 'uid'=>$this->params['uid']]);
+            } catch(\Exception $e){
+                return _error($e->getMessage(), $e->getCode());
+            }
+            return _success($status);
+        }
+    }
+
+    public function guideAudit(){
+        if(IS_AJAX){
+            try{
+                $this->guide->update(['status'=>$this->params['status'], 'uid'=>$this->params['uid']]);
+            } catch (\Exception $e){
+                return _error($e->getMessage(), $e->getCode());
+            }
+            return _success();
+        }
     }
 
 
