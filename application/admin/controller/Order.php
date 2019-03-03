72976cc5911ff18f2b2fc2efc5f275493e2be276
SenCaoKing <1134349927@qq.com>
2019-03-03 23:26:38 +0800

    订单列表

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -1,13 +1,36 @@
 <?php
+/**
+ * created by PhpStrom
+ * User: Sen
+ * Date: 2018/02/14
+ * Time: 14:00
+ */
 namespace app\admin\controller;
+use app\server\Param;
+use app\server\Alioss;
 
-use think\Controller;
+class Order extends Base{
+    protected $model;
 
-class Order extends Controller
-{
-    public function orderlist()
-    {
-        dump(123);
-        return view('orderlist');
+    public function __construct(){
+        parent::__construct();
+    }
+
+    /**
+     * 订单列表
+     * @return \think\response\View
+     */
+    public function orderlist(){
+        $search = search($this->params, ['a.order_num'], ['a.create_time'], ['a.status']);
+        $where = $search['where'] ?: '';
+
+        $list = $this->order
+            ->alias('a')
+            ->field('a.*, b.goods_id, c.title')
+            ->join([['goods_release b', 'a.goods_release_id = b.id', 'left'], ['goods c', 'b.goods_id = c.id', 'left']])
+            ->where($where)
+            ->order('a.id desc')
+            ->paginate(10, false, ['query'=>$this->params, 'var_page'=>'page']);
+        return view('orderList', ['list'=>$list, 'params'=>$search['params']]);
     }
 }
