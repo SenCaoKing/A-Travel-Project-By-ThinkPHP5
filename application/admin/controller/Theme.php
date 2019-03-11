819bbcdbfb4ddd021c01f8604156aa95f89daca8
SenCaoKing <1134349927@qq.com>
2019-03-11 19:51:32 +0800

    话题修改

--- application/admin/controller/Theme.php
+++ application/admin/controller/Theme.php
@@ -63,4 +63,30 @@ class Theme extends Base{
         $theme_label = explode(',', $config['theme_label']);
         return view('themeAdd', ['circle_id'=>$circle_id, 'theme_label'=>$theme_label]);
     }
+
+    /**
+     * 编辑
+     * @return \think\response\Json|\think\response\View
+     */
+    public function themeSave(){
+        if(IS_POST){
+            try{
+                // 场景验证
+                $validate = $this->validate($this->params, 'Theme.save');
+                if(true !== $validate){
+                    throw new \LogicException($validate, 1000);
+                }
+
+                $this->theme->update($this->params);
+            } catch (\Exception $e){
+                return _error($e->getMessage(), $e->getCode());
+            }
+            return _success();
+        }
+        $info = $this->theme->where(['id'=>$this->params['id']])->find();
+        $info['label'] = $this->theme_label->where(['theme_id'=>$this->params['id']])->column('theme_label');
+        $config = get_config();
+        $theme_label = explode(',', $config['theme_label']);
+        return view('themeSave', ['info'=>$info, 'theme_label'=>$theme_label]);
+    }
 }
